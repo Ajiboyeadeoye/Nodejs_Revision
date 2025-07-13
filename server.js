d2283b771a26e3775ad475643a5ebc891c028acc
Ajiboye Adeoye <ajiboyeadeoye25@yahoo.com>
2025-07-13 17:34:09 +0100

    more on installing packages locally into the project folder, eg, lodash- got methods!

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const http = require("http");
 const fs = require("fs");
+const _ = require("lodash");
+
+
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+    // lodash
+    const num = _.random(0, 20);
+    console.log(num);
+    
+const greet = _.once((name) => {
+    console.log(`hello, ${name}`)
+})
+    
+   greet("Adeoye")
     
     // set header content type
     res.setHeader('content-Type', 'text/html');
 
+    
     // res.write('I love this website im creating');
     // res.write('<p>Hello Mr Ajboye</p>')
     // res.write('<p>Hello Mr Ajboye again</p>')
@@ -16,12 +28,20 @@ const server = http.createServer((req, res) => {
     switch(req.url) {
         case '/':
             path += 'index.html';
+            res.statusCode = 200;
             break;
         case '/about':
             path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about')
+            res.end();
             break;
         default:
             path += '404.html';
+            res.statusCode = 404;
             break;
     }
 
