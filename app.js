b671d8d48e36485c327c03cbce094e9a44daf06a
Ajiboye Adeoye <ajiboyeadeoye25@yahoo.com>
2025-07-26 17:12:30 +0100

    different methods on mongoose used and added, knownledge of _id fully explained

--- app.js
+++ app.js
@@ -2,18 +2,19 @@ const express = require("express");
 const morgan = require("morgan");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 
 
 
 const app = express();
 
-const { dbURI } = process.env;
+const { dbURL, dbURL1 } = process.env;
 
 // connect to database
 const connectTodb = async () => {
     try {
-     await mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+     await mongoose.connect(dbURL1, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("connected to db")
     }catch (error){
         throw Error ("unable to connect to db")
@@ -38,18 +39,46 @@ app.set("view engine", "ejs");
 // middleware and static files
 app.use(express.static("public"));
 app.use(morgan("tiny"));
+app.use(express.urlencoded({ extended: true }));
 
+// mongoose and mongo sandbox routes
+// app.get("/add-blog", (req, res) => {
+//     const blog = new Blog({
+//         title: "new blog 2",
+//         snippet: "about my new blog",
+//         body: "More about my new blog"
+//     });
+//     blog.save()
+//        .then((result) => {
+//         res.send(result);
+//        })
+//        .catch((err) => {
+//         console.log(err);
+//        });
+// });
+
+// app.get("/all-blogs", async (req, res) => {
+//     let allBlogs;
+//     try {
+//         allBlogs = await Blog.find();
+//     } catch (err) {
+//         throw err;
+//     }
+//      res.send(allBlogs);
+// });
+
+// app.get("/single-blog", (req, res) => {
+//     Blog.findById("687cee73e49a70ac1f63b585")
+//         .then((result) => {
+//             res.send(result);
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         })
+// });
 
 app.get("/", (req, res) => {
-    const blogs = [
-    { title : "Yoshi fnds eggs", snippet: "Lorem ipsum dolor sit among the orator"},
-    { title : "Kenny fnds yam", snippet: "Lorem ipsum dolor sit among the orator"},
-    { title : "Adeoye finds rice", snippet: "Lorem ipsum dolor sit among the orator"}
-    ];
-    //res.send('<p>Home page</p>')
-    //res.sendFile("./views/index.html", { root: __dirname})
-    // Now rendering
-    res.render("index", { title: "Home", blogs});
+   res.redirect("/blogs")
 });
 
 // app.use((req, res, next) => {
@@ -72,9 +101,59 @@ app.get("/about", (req, res) => {
 //     res.redirect("/about")
 // })
 
+// blog routes
+app.get("/blogs", (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+        .then((result) => {
+            res.render("index", { title: "All Blogs", blogs: result })
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+});
+
+app.post("/blogs", (req, res) => {
+    const blog = new Blog (req.body);
+
+    blog.save()
+        .then((result) => {
+            res.redirect("/blogs")
+        })
+        .catch((err) => {
+            console.log(err)
+        }) 
+});
+
+
 app.get("/blogs/create", (req, res) => {
     res.render("create", { title: "New blog"});
+});
+
+
+app.get("/blogs/:id", (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+        .then(result => {
+            res.render("details", { blog: result, title: "Blog Details" })
+        })
+        .catch(err => {
+            console.log(err)
+        });
 })
+
+
+
+
+//  const blogs = [
+//     { title : "Yoshi fnds eggs", snippet: "Lorem ipsum dolor sit among the orator"},
+//     { title : "Kenny fnds yam", snippet: "Lorem ipsum dolor sit among the orator"},
+//     { title : "Adeoye finds rice", snippet: "Lorem ipsum dolor sit among the orator"}
+//     ];
+//     //res.send('<p>Home page</p>')
+//     //res.sendFile("./views/index.html", { root: __dirname})
+//     // Now rendering
+//     res.render("index", { title: "Home", blogs});
+
 
 app.use((req, res) => {
     //res.send('<p>About page</p>')
