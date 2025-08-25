1a05eeecfb6e2aa81f01365cf3765c3df42b5b55
Ajiboye Adeoye <ajiboyeadeoye25@yahoo.com>
2025-08-25 15:41:27 +0100

    The simple blog web app fully ready, material icon implemented, MVC concept used

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,10 +1,11 @@
 const Blog = require("../models/blog");
+
 // blog_index, blog_details, blog_create_get, blog_create_post, blog_delete
 
 const blog_index = (req, res) => {
     Blog.find().sort({ createdAt: -1 })
             .then((result) => {
-                res.render("index", { title: "All Blogs", blogs: result })
+                res.render("blogs/index", { title: "All Blogs", blogs: result })
             })
             .catch((err) => {
                 console.log(err);
@@ -15,15 +16,15 @@ const blog_details = (req, res) => {
     const id = req.params.id;
     Blog.findById(id)
         .then(result => {
-            res.render("details", { blog: result, title: "Blog Details" })
+            res.render("blogs/details", { blog: result, title: "Blog Details" })
         })
         .catch(err => {
-            console.log(err)
+            res.render("404", { title: "Blog not found" });
         });
 }
 
 const blog_create_get = (req, res) => {
-    res.render("create", { title: "New blog"});
+    res.render("blogs/create", { title: "New blog"});
 }
 
 const blog_create_post = (req, res) => {
